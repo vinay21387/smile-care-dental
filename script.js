274a0ce8768994cfd8995ec64f9386b04e9811af
Vinay Kumar Malik <107244657+vinay21387@users.noreply.github.com>
2024-09-15 21:36:30 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+document.querySelector('.appointment').addEventListener('click', () => {
+    alert('Redirecting to Book Appointment page');
+});
+
+document.querySelector('.help').addEventListener('click', () => {
+    alert('How can we help you today?');
+});
+
+document.querySelector('.book-now').addEventListener('click', () => {
+    alert('Redirecting to Book Appointment page');
+});
